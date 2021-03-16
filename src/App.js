a8616acfdbac37724d803c45e8e66b977df9b24d
Ayush Bajracharya <bajracharya.ayush@webexpertsnepal.com>
2021-03-16 09:58:47 +0545

    markup initialized for the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import Balance from './Components/Balance'
+import IncomeExpense from './Components/IncomeExpense'
+import TransactionList from './Components/TransactionList'
+import AddTransaction from './Components/AddTransaction'
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Header />
       <div className = "container">
         <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
